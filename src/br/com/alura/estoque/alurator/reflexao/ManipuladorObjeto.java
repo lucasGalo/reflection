eb729d47b13b387d937cd9f4e464a06fcdbc1942
lucas_galo <lucas_d>
2020-12-30 10:47:41 -0300

    no inteli j tem q configurar para o java obter os nomes dos métodos originais

--- src/br/com/alura/estoque/alurator/reflexao/ManipuladorObjeto.java
+++ src/br/com/alura/estoque/alurator/reflexao/ManipuladorObjeto.java
@@ -11,25 +11,30 @@ public class ManipuladorObjeto {
         this.instancia = instancia;
     }
 
-    public ManipuladorMetodo getMetodo(String nomeMetodo, Map<String, Object> parans) {
+    public ManipuladorMetodo getMetodo(String nomeMetodo, Map<String, Object> params) {
         // 1) Pegar todos os métodos da classe.
         // 2) Filtrar todos os métodos de modo que:
         // 2.1) Tenham o mesmo nome informado pelo usuário;
         // 2.2) Tenham a mesma quantidade de parâmetros passados na URL;
         // 2.3) E que cada um dos parâmetros tenham os mesmos nomes e tipos iguais aos passados na URL.
         // 3) Lançar uma RuntimeException caso nenhum método seja encontrado.
+        // /produto/filtra?nome=produto&marca=marca 1
 
-        Stream<Method> methods = Stream.of(instancia.getClass().getDeclaredMethods());
-        Method metodoSelecionado = methods.filter(metodo -> true)
+        Stream<Method> metodos = Stream.of(instancia.getClass().getDeclaredMethods());
+        Method metodoSelecionado = metodos.filter(metodo ->
+                metodo.getName().equals(nomeMetodo)
+                        && metodo.getParameterCount() == params.values().size()
+                        && Stream.of(metodo.getParameters())
+                        .allMatch(arg -> {
+                            System.out.println(arg.getName());
+                         return   params.keySet().contains(arg.getName())
+                                    && params.get(arg.getName()).getClass().equals(arg.getType());
+                        })
+        )
                 .findFirst()
                 .orElseThrow(() -> new RuntimeException("Método não encontrado"));
-        try {
-            Method metodo = instancia.getClass().getDeclaredMethod(nomeMetodo);
-            return new ManipuladorMetodo(instancia, metodo);
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
+
+        return new ManipuladorMetodo(instancia, metodoSelecionado, params);
 
     }
 }
