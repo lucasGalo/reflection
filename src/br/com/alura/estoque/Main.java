7e069c85763117fe6e5b87d704879a5aaa237b10
lucas_galo <lucas_d>
2021-05-11 20:06:56 -0300

    add nova rota

--- src/br/com/alura/estoque/Main.java
+++ src/br/com/alura/estoque/Main.java
@@ -10,28 +10,29 @@ public class Main {
 
 	/**
 	 * Simula o navegador.
-	 * 
+	 *
 	 */
 	public static void main(String[] args) throws Exception {
-		
+
 		/*
 		 * Casos possiveis:
 		 * /controlador/metodo
 		 * /controlador/metodo?param1=valor1&param2=valor2
+		 * /produto/filtra?nome=produto&marca=marca1
 		 */
-		
+
 		try (Scanner s = new Scanner(System.in)) {
 			String url = s.nextLine();
-			
+
 			Alurator alurator = new Alurator("br.com.alura.estoque.controle.");
 			alurator.registra(ProdutoDao.class, ProdutoDaoMock.class);
 			// não consigo add tipos diferentes
 //			alurator.registra(List.class, String.class);
 			while (!url.equals("exit")) {
 				Object response = alurator.executa(url);
-				
+
 				System.out.println("Response: " + response);
-				
+
 				url = s.nextLine();
 			}
 		}
